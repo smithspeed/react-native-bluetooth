9c2f64fb960972443e09cfb9a66ad2950c467706
smithspeed <dhananjayspeed@gmail.com>
2023-04-09 14:40:33 +0530

    feat: upds

--- ios/Bluetooth-Bridging-Header.h
+++ ios/Bluetooth-Bridging-Header.h
@@ -1,2 +1,5 @@
 #import <React/RCTBridgeModule.h>
 #import <React/RCTViewManager.h>
+#import <React/RCTLog.h>
+#import <React/RCTEventEmitter.h>
+//#import <CoreBluetooth/CoreBluetooth.h>
--- ios/Bluetooth.m
+++ ios/Bluetooth.m
@@ -1,14 +1,22 @@
 #import <React/RCTBridgeModule.h>
+#import <React/RCTLog.h>
+#import "React/RCTEventEmitter.h"
+//#import <CoreBluetooth/CoreBluetooth.h>
 
-@interface RCT_EXTERN_MODULE(Bluetooth, NSObject)
+@interface RCT_EXTERN_MODULE(Bluetooth, RCTEventEmitter)
 
 RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
 
-+ (BOOL)requiresMainQueueSetup
-{
-  return NO;
-}
+/*RCT_EXTERN_METHOD(bluetoothStatus:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject
+                  )*/
+
+RCT_EXTERN_METHOD(bluetoothStatus:(NSString)a
+                  withResolver:(RCTPromiseResolveBlock)resolve
+                  withRejecter:(RCTPromiseRejectBlock)reject
+                  )
+
 
 @end
